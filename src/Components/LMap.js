00334326385b696db43a4151a8175ceecd0ce5e3
Laura <laurav.zarco@gmail.com>
2018-09-06 18:24:40 +0200

    unseen save

--- src/Components/LMap.js
+++ src/Components/LMap.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import Poi from './Poi';
 
 
-class Map extends Component {
+class LMap extends Component {
     render() {
         return (
             <div>
-
                 <Poi />
             </div>
         );
     }
 }
 
-export default Map;
+export default LMap;
