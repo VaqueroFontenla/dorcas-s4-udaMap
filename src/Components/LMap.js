a5f8de7f75180839f0897caec4e7fe9f83f00bbb
Loreto Vaquero <loret.vaquero@gmail.com>
2018-09-14 16:45:51 +0200

    First try

--- src/Components/LMap.js
+++ src/Components/LMap.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import L from 'leaflet';
+import './styleIndex.css';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import Poi from './Poi';
 
-
 class LMap extends Component {
-    render() {
-        return (
-            <div>
-                <Poi />
-            </div>
-        );
-    }
+  constructor (props) {
+    super(props);
+    this.state = {
+     lat: 51.505,
+     lng: -0.09,
+     zoom: 13,
+   };
+  }
+  render() {
+    const position = [this.state.lat, this.state.lng]
+    return (
+      <Map className= "MapStyle" center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    )
+  }
 }
 
 export default LMap;
