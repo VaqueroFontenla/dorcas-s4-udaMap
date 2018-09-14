c9e8c66be861a9f6030286cb66031d014097160f
Loreto Vaquero <loret.vaquero@gmail.com>
2018-09-14 20:50:36 +0200

    Add Pop Up Styles

--- src/components/LMap/LMap.js
+++ src/components/LMap/LMap.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import { PopUp } from './styleLMap'
+import { PopUp, TitlePopup, TextStyle, Operation, TextStyleBold } from './styleLMap'
 
 
 class LMap extends Component {
@@ -12,11 +12,22 @@ class LMap extends Component {
 
   render() {
     const MapStyle = this.props.styles;
+    const data = this.props.data;
     const position = [this.props.coordinates.lat, this.props.coordinates.lon]
     console.log(MapStyle)
     const iconMarker = new L.Icon({
       iconUrl: require('../../images/poi.png')
     })
+
+    let operation;
+    if(data.operation === 0) {
+      operation = 'Rent'
+    } else if (data.operation === 1) {
+      operation = 'Sale'
+    } else {
+      return null;
+    }
+
     return (
       <Map style={MapStyle} center={position} zoom={this.props.coordinates.zoom}>
         <TileLayer
@@ -26,9 +37,24 @@ class LMap extends Component {
         <Marker position={position} icon={iconMarker}>
           <Popup>
             <div style={PopUp}>
-              <span>Dirección<br />
-                <span style={{ display: 'block', textAlign: 'center' }}><b>Precio</b> €</span>
-              </span>
+              <div style={TitlePopup}>
+                <span>Information</span>
+              </div>
+              <div style={{display:'flex', flexDirection:'column', justifyContent:'left'}}>
+                <div>
+                  <span style={TextStyleBold}>Address:  </span>
+                  <span style={TextStyle}>{data.address}</span>
+                </div>
+                <div>
+                  <span style={TextStyleBold}>udaValue:  </span>
+                  <span style={TextStyle}>{data.uDAVALUE}</span>
+                  <span style={TextStyleBold}>  €</span>
+                </div>
+                <div>
+                  <span style={TextStyleBold}>Operation:  </span>
+                  <span style={Operation}>{operation}</span>
+                </div>
+              </div>
             </div>
           </Popup>
         </Marker>
--- src/components/LMap/styleLMap.js
+++ src/components/LMap/styleLMap.js
@@ -1,11 +1,31 @@
 const PopUp = {
-  height:'180px',
+  height:'80px',
   width: '300px',
-  border:'2px solid #CA1C24',
-  borderRadius:'10px',
-  backgroundColor: '#EFF2F7',
   color: '#07090D',
-  
 }
 
-export { PopUp };
+const TitlePopup = {
+  margin:'10px',
+  width: '100%',
+  fontFamily: 'Gotham-Bold, sans serif',
+  textAlign: 'center',
+}
+
+const TextStyle = {
+  fontFamily: 'Gotham-Light, sans serif',
+}
+
+const TextStyleBold = {
+  fontFamily: 'Gotham-Bold, sans serif',
+  fontWeight: 'bold',
+  color: '#07090D',
+}
+
+const Operation = {
+  fontFamily: 'Gotham-Bold, sans serif',
+  color: 'red',
+  marginLeft: '2px',
+  fontWeight: 'bold',
+}
+
+export { PopUp, TitlePopup, TextStyle, TextStyleBold, Operation };
