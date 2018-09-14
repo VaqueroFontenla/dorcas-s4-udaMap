5aeb3664cfe9157e3d22c0fc684f64ee1bfa9903
Laura <laurav.zarco@gmail.com>
2018-09-14 18:08:55 +0200

    Poi with styles

--- src/components/LMap/LMap.js
+++ src/components/LMap/LMap.js
@@ -1,28 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import Poi from '../Poi/Poi';
 import { MapStyle } from './styleLMap.js';
+import L from 'leaflet';
+
 
 class LMap extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-     lat: 51.505,
-     lng: -0.09,
-     zoom: 13,
-   };
+      lat: 40,
+      lng: -3,
+      zoom: 13,
+    };
   }
   render() {
     const position = [this.state.lat, this.state.lng]
+    const iconMarker = new L.Icon({
+      iconUrl: require('../../images/poi.png')
+    })
     return (
-      <Map style= {MapStyle} center={position} zoom={this.state.zoom}>
+      <Map style={MapStyle} center={position} zoom={this.state.zoom}>
         <TileLayer
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={position} icon={iconMarker}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            <span>Dirección<br />
+              <span style={{ display: 'block', textAlign: 'center' }}><b>Precio</b> €</span>
+            </span>
           </Popup>
         </Marker>
       </Map>
